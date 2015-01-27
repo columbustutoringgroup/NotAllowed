4fec0e3af9e87d221bba93dc1400f9dc15597c50
Robert Lude <rlude@covermymeds.com>
2015-01-27 15:14:46 -0500

    added admin flag to users

--- source/db/schema.rb
+++ source/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127195616) do
+ActiveRecord::Schema.define(version: 20150127200954) do
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150127195616) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
