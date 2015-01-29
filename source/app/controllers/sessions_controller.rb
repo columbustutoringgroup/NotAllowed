2846917b7f71ed407c401e6e1df4dae6f706c943
Nicolas McCurdy <thenickperson@gmail.com>
2015-01-29 13:57:55 -0500

    Implement login and add columns to users

--- source/app/controllers/sessions_controller.rb
+++ source/app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash.notice = "Success"
+    else
+      flash.notice = "Failure"
+    end
+    redirect_to root_url
   end
 
   def destroy
