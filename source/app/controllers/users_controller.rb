abe4c6bb4d3e10b4e2c7c33c068eadc07f0b360a
Nicolas McCurdy <thenickperson@gmail.com>
2015-01-29 14:15:24 -0500

    Implement sign up

--- source/app/controllers/users_controller.rb
+++ source/app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def create
+    if @user = User.create(user_params)
+      flash.notice = "Success"
+      session[:user_id] = @user.id
+    else
+      flash.notice = "Failure"
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
