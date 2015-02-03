71a4eec60f6b2f7bdd2397a7a82777f40e369397
Mike Kenworthy <MKenworthy@covermymeds.com>
2015-02-03 17:12:57 -0500

    current site

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+
+  before_save :encrypt_password
+
+  validates :password, presence: { on: :create }, confirmation: true
+  validates :email, presence: true, uniqueness: true
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    user if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 end
--- source/db/schema.rb
+++ source/db/schema.rb
@@ -11,11 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025010015) do
+ActiveRecord::Schema.define(version: 20150202171513) do
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
   end
 
 end
