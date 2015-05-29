de2beed94a96ede612587cb1a8b589437953ab96
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-29 16:14:54 -0400

    Basic auth functionality

--- source/app/controllers/application_controller.rb
+++ source/app/controllers/application_controller.rb
@@ -2,13 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :logged_in?, :current_user
+
+  def authenticate_user
+    redirect_to "sessions#new" if !logged_in?
+  end
+
+  private
 
   def logged_in?
+    session[:user_id].present?
   end
 
   def current_user
-  end
-
-  def authenticate_user
+    User.find(session[:user_id]) if session[:user_id]
   end
 end
--- source/app/controllers/sessions_controller.rb
+++ source/app/controllers/sessions_controller.rb
@@ -4,9 +4,28 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: session_params[:email]).try(:authenticate, session_params[:password])
+
+    if user
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash.now[:alert] = "Email or password incorrect!"
+      render 'new'
+    end
   end
 
   def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+    end
+
+    redirect_to root_path
   end
 
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
