27fdfe30c36ef3e82201100cc59f1a3effd0634d
Lovinder Pnag <lspnag@gmail.com>
2015-06-01 00:26:01 -0400

    added a bunch of files for user sign up and sessin creation

--- source/app/controllers/users_controller.rb
+++ source/app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  # before_filter :redirect_if_logged_in
+
   def show
+    
+  end
+
+  def new
+    @user = User.new  
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def redirect_if_logged_in
+    redirect_to root_path if logged_in?
   end
 end
--- source/config/routes.rb
+++ source/config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'sessions#index'
 
+
+  post "/login" => "sessions#create", as: "login"
+  get "/login" => "sessions#new"
+  delete "/logout" => "sessions#destroy", as: "logout"
+
+  resources :users, only: [:index, :new, :create]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
