b032125c946bc729cba51aceca0e8f5f41ebb4d1
Vasanth <vasanth.pappu@gmail.com>
2015-08-22 20:51:11 -0400

    added csrf protection back in used token_tag to generate authenticity token in form

--- source/app/controllers/application_controller.rb
+++ source/app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :null_session
 
-  #protect_from_forgery with: :null_session
   before_action :authenticate_user
 
   def logged_in?
+    session[:logged_in]
   end
 
   def current_user
