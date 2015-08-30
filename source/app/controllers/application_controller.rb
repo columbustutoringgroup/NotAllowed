f29cf6fdc55a661f172fa090e6c9268c81c6c302
Vasanth <vasanth.pappu@gmail.com>
2015-08-29 20:02:14 -0400

    auth logic

--- source/app/controllers/application_controller.rb
+++ source/app/controllers/application_controller.rb
@@ -6,16 +6,32 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user
 
   def logged_in?
-    session[:logged_in]
+    not session[:user_id].nil?
   end
 
   def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
   end
+  helper_method :current_user
 
   def authenticate_user
     unless logged_in?
-      flash[:error] = "Please log in first"
-      redirect_to '/sessions#index'
+      puts 'NOT LOGGED IN'
+      user = User.find_by_email(params[:email])
+      puts user
+      puts params[:password] + " is the password"
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        puts 'AUTHENTICATION FAILED'
+        flash[:error] = "Please log in first"
+        redirect_to '/sessions#index'
+      end
+    else
+      puts 'ALREADY LOGGED IN'
     end
   end
 
