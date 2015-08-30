123e3aec5bd8640efb1581c60c1272efdab533c6
Vasanth <vasanth.pappu@gmail.com>
2015-08-29 20:03:23 -0400

    migrate user model with email and digest

--- source/app/controllers/users_controller.rb
+++ source/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def create
+    user = User.create(
+      email: params[:email],
+      password: params[:password],
+    )
+    user.save!
+    redirect_to root_path
+  end
 end
