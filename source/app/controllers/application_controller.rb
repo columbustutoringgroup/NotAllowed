ee13650ca5cc5ac2df250a5675da790b4961ece2
Joe Reich <jreich@CMDVMOSXLT126.local>
2015-11-30 09:13:35 -0500

    Added signup functionality

--- source/app/controllers/application_controller.rb
+++ source/app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def logged_in?
-  end
+  # def logged_in?
+  #   false
+  # end
+  # helper_method :logged_in?
+
+  include ApplicationHelper
 
   def current_user
   end
--- source/app/controllers/users_controller.rb
+++ source/app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user && @user.save
+      redirect_to @user
+    else
+      redirect_to root_url
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
