9eb86ca2be97d8e1d58c358bc2940d98fb5ded52
Joe Reich <jreich@CMDVMOSXLT126.local>
2015-12-02 23:03:47 -0500

    halfway through login

--- source/app/controllers/sessions_controller.rb
+++ source/app/controllers/sessions_controller.rb
@@ -4,9 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      current_user = user
+      
+      redirect_to user
+    else
+      render 'index'
+    end
   end
 
   def destroy
   end
+
+  # def log_in
+  #   session[user_id] = user.id
+  # end
+
+  # def 
 
 end
--- source/config/routes.rb
+++ source/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'sessions#index'
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:create, :destroy, :index]
   resources :users, only: [:new, :create, :show]
 
 
