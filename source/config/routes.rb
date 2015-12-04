da7f0a901370218d60bc3fc102aba143998e1fef
Joe Reich <jreich@CMDVMOSXLT126.local>
2015-12-03 23:10:37 -0500

    added secret admin page

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'sessions#index'
   delete 'logout' => 'sessions#destroy'
+  get 'admin' => 'admin#show'
   resources :sessions, only: [:create, :destroy, :index]
   resources :users, only: [:new, :create, :show]
 
