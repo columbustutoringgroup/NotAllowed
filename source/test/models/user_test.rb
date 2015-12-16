824f53da95c1ad93de3a0640e6ac186781c788d5
Samuel Priddy <spriddy@covermymeds.com>
2015-12-16 16:11:27 -0500

    Add has_secure_password to User and break apart entries in UserTest for better debugging

--- source/test/models/user_test.rb
+++ source/test/models/user_test.rb
@@ -1,25 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid if there is no password" do
+    assert_not User.new({user_name: 'Bill'}).valid?
+  end
 
+  test "invalid if password is too short" do
+    assert_not User.new({user_name: 'Bill', password: '1234'}).valid?
+  end
 
-  test "when saving a new user" do
-    # Fails if there is no password
-    assert_not User.new({user_name: 'Bill'}).save
-
-    # Fails if the password is too short
-    assert_not User.new({user_name: 'Bill', password: '1234'}).save
-
-    # Succeeds when given a valid password
+  test "valid when given a valid password" do
     valid_passwords = ['asdoifho132',
                        '12391987879',
                        'the yellow badger ate the moon']
 
     valid_passwords.each do |pass|
-      assert User.new({user_name: 'Bill', password: pass}).save
+      assert User.new({user_name: 'Bill', password: pass}).valid?
     end
   end
 end
