df77113d36b7ec73c0e04ae044c29fd40b57b813
Samuel Priddy <spriddy@covermymeds.com>
2015-12-16 16:05:09 -0500

    Add tests for User#save for good/bad passwords

--- source/test/models/user_test.rb
+++ source/test/models/user_test.rb
@@ -4,4 +4,22 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "when saving a new user" do
+    # Fails if there is no password
+    assert_not User.new({user_name: 'Bill'}).save
+
+    # Fails if the password is too short
+    assert_not User.new({user_name: 'Bill', password: '1234'}).save
+
+    # Succeeds when given a valid password
+    valid_passwords = ['asdoifho132',
+                       '12391987879',
+                       'the yellow badger ate the moon']
+
+    valid_passwords.each do |pass|
+      assert User.new({user_name: 'Bill', password: pass}).save
+    end
+  end
 end
