d34db30d41350fa1fbb3de7503f6261b303d5acc
Samuel Priddy <spriddy@covermymeds.com>
2015-12-17 00:25:19 -0500

    Add routes

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'sessions#index'
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
