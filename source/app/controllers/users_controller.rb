1d76e78e50a73afe45bccf112a15898c4a50b3c3
kmodlich <modlich.14@osu.edu>
2016-06-02 11:03:11 -0400

    Everything works?

--- source/app/controllers/users_controller.rb
+++ source/app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		session[:current_user_id] = @user.id
+  		redirect_to root_path
+  	else
+  		flash[:sign_up_error] = "An error occured while signing up."
+  		redirect_to new_user_path
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name,:password,:password_confirmation)
   end
 end
---
+++ source/db/migrate/20160602133233_update_password_name.rb
@@ -0,0 +1,5 @@
+class UpdatePasswordName < ActiveRecord::Migration
+  def change
+  	rename_column :users, :password, :password_digest
+  end
+end
